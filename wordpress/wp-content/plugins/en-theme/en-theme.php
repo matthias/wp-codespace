0e11e075fa6471a7de9c34ed91df370d51d87887
Matthias Platzer <matthias.platzer@gmail.com>
2023-02-12 15:53:48 +0000

    Initial commit of en-theme

---
+++ wordpress/wp-content/plugins/en-theme/en-theme.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Helper plugin for developing en-themes
+ *
+ * @package WP_Codespace
+ * @version 0.0.1
+ */
+
+/*
+Plugin Name: EN-Theme
+Description: Helper plugin for developing en-themes.
+Author: EN GARDE
+License: ©2023
+*/
+
+
+
+
